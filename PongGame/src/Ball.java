52c36f3aee573b532a63cd6603fe0937bd220180
vagnerbauer <vagnerbauer@gmail.com>
2022-05-17 22:35:25 +1200

    Changed ball colour to green instead of white

--- PongGame/src/Ball.java
+++ PongGame/src/Ball.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 public class Ball extends Sprite {
 	private static final int BALL_WIDTH = 25;
 	private static final int BALL_HEIGHT = 25;
-	private static final Color BALL_COLOUR = Color.WHITE;
+	private static final Color BALL_COLOUR = Color.GREEN;
 
 	public Ball(int panelWidth, int panelHeight) {
 		setWidth(BALL_WIDTH);
